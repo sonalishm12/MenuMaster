0bc8f236a288224c78140dbdd599eb554023d896
sonali sharma <160767577+sonalishm12@users.noreply.github.com>
2024-02-22 10:53:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Resturant from './component/basics/resturant'
+import { useState } from 'react';
+const App = () => {
+  return  <Resturant/>;
+  
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import './App.css';
+
+// const App = () => {
+//   return(
+//     <>
+//     <MyWork/>
+//     <MyWork/>
+//     <h1 className='classname'>Hello World </h1>
+//     </>
+//   );
+// };
+
+// const MyWork = () => {
+//    return(
+//     <h1> welcome to my work </h1>
+//     );
+// };
+
+// export default App;
